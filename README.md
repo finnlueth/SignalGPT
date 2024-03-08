246fec192f96550bed52371c60e70cc9e7655fd2
Finn Lueth <finn@lueth.net>
2024-03-08 15:31:00 +0000

    changes

--- README.md
+++ README.md
@@ -4,3 +4,16 @@
 
 SignalGPT aims to enhance the prediction of signal peptides by using large language models, specifically focusing on the Low-Rank Adaptation technique to efficiently fine-tune an array of domain expert pre-trained model adapters.
 It matches the performance of previous methods while being lightweight and inexpensive to train.
+
+
+
+## EC2 Commands
+sudo yum install git -y
+sudo yum install make
+sudo yum install python311
+nano ~/.bashrc
+sudo yum -y install python-pip
+pip install poetry
+
+git config --global user.name "Finn Lueth"
+git config --global user.email "finn@lueth.net"
--- notebooks/deprecated_code.py
+++ notebooks/deprecated_code.py
@@ -3,3 +3,17 @@ params['crf.modules_to_save.default._constraint_mask']
 
 
 # print(*params.keys(), sep='\n')
+
+aaa = torch.nn.Parameter(torch.tensor([[1., 0., 1., 0., 0., 0., 0., 0., 1.],
+        [0., 1., 0., 1., 0., 0., 0., 0., 0.],
+        [1., 0., 1., 1., 0., 0., 0., 0., 1.],
+        [0., 0., 1., 1., 0., 0., 0., 0., 1.],
+        [0., 0., 0., 1., 1., 0., 0., 0., 0.],
+        [0., 0., 0., 1., 0., 1., 0., 0., 0.],
+        [0., 0., 0., 1., 0., 0., 1., 0., 0.],
+        [1., 1., 0., 1., 1., 1., 1., 0., 0.],
+        [0., 0., 0., 0., 0., 0., 0., 0., 0.]]), requires_grad=False)
+
+t5_base_model.crf.original_module._constraint_mask, t5_base_model.crf.original_module.tensor_constraint_mask
+
+t5_base_model.crf.modules_to_save.default._constraint_mask, t5_base_model.crf.modules_to_save.default.tensor_constraint_mask
